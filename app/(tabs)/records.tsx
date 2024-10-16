7a2c914032b522fa2ade6e4d73254709e4300f09
MiriamPhiri <bsc-com-13-22@unima.ac.mw>
2024-10-16 18:58:04 +0200

    added the records page

---
+++ app/(tabs)/records.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function Records(){
+
+    return(
+        <View className='h-full lex flex-col items-center justify-center bg-green-600'>
+        <Text className="text-white">
+            Records
+        </Text>
+        </View>
+    )
+}
