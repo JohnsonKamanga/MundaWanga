aa84d4851817f48d66a63c666d629cc2df0c63ab
JohnsonKamanga123 <bsc-com-09-21@unima.ac.mw>
2024-11-08 16:12:45 +0200

    created the settings component

---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function Settings(){
+
+    return(
+        <View className="min-h-screen h-full w-full bg-red=500">
+            <Text className="text-white">
+                Your Settings
+            </Text>
+        </View>
+    )
+}
