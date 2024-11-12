947ae98b60848bf0944d756700cd4e87c0c8f3bc
JohnsonKamanga123 <bsc-com-09-21@unima.ac.mw>
2024-11-12 20:15:51 +0200

    added styling to the sidebar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,20 +1,36 @@
+import { Colors } from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
+import { useColorScheme } from '@/hooks/useColorScheme';
 import {Drawer} from 'expo-router/drawer';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function SideBar(){
-
+  const colorScheme = useColorScheme()
   return(
     <GestureHandlerRootView>
       <Drawer
+      
       screenOptions={{
         headerStatusBarHeight: 20,
+        drawerActiveTintColor: 'white',
+        drawerInactiveTintColor: 'rgb(230,230,230)',
+        drawerStyle:{
+          backgroundColor: Colors[colorScheme ?? 'light'].barColor,
+          borderColor: 'rgba(256,256,256,0.2)',
+          borderRightWidth: 0.3,
+        },
+        headerTitleAlign: 'center',
+        headerTintColor: 'white',
+        headerStyle: {
+          backgroundColor: Colors[colorScheme ?? 'light'].barColor
+        }
       }}
       >
         <Drawer.Screen
         name='(home)'
         options={{
           drawerLabel: 'Home',
-          title: 'Home-title',
+          title: 'MundaWanga',
         }}
         />
         <Drawer.Screen
