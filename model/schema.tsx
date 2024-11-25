a8ebeef75ac9da67947133222e2a7321d64d4c64
1234567abule <bsc-45-21@unima.ac.mw>
2024-11-25 23:04:18 +0200

    Creating schema

--- model/schema.tsx
+++ model/schema.tsx
@@ -3,18 +3,6 @@ import * as FileSystem from "expo-file-system";
 import * as SQLite from "expo-sqlite";
 import { Asset } from "expo-asset";
 
-// async function openDatabase(pathToDatabaseFile: string): Promise<SQLite.WebSQLDatabase> {
-//   if (!(await FileSystem.getInfoAsync(FileSystem.documentDirectory + 'SQLite')).exists) {
-//     await FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'SQLite');
-//   }
-//   const asset = await Asset.fromModule(require(pathToDatabaseFile)).downloadAsync();
-//   await FileSystem.copyAsync({
-//     from: asset.localUri,
-//     to: FileSystem.documentDirectory + 'SQLite/myDatabaseName.db',
-//   });
-//   return openDatabase('myDatabaseName.db');
-// }
-
 export const db = SQLite.openDatabaseAsync("MundaWanga.db");
 
 export function createBudgetTable(db: SQLiteDatabase): Promise<any> {
@@ -27,21 +15,5 @@ export function createBudgetTable(db: SQLiteDatabase): Promise<any> {
     end_date DATETIME NOT NULL , 
     max_amount REAL NOT NULL , 
     used REAL DEFAULT 0 NOT NULL );
-        `);
-}
-
-export function createRecordTable(db: SQLiteDatabase): Promise<any> {
-  return db.runAsync(`
-      CREATE TABLE IF NOT EXISTS  record (
-      id INTEGER PRIMARY KEY NOT NULL , 
-      fields TEXT NOT NULL );
-          `);
-}
-
-export function createRecordSchemaTable(db: SQLiteDatabase): Promise<any> {
-  return db.runAsync(`
-      CREATE TABLE IF NOT EXISTS  record_schema (
-      id INTEGER PRIMARY KEY NOT NULL , 
-      fields TEXT NOT NULL );
-          `);
+       `);
 }
