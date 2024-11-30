664eb8b659726ff8ce2eb2bf8389def87271515d
1234567abule <bsc-45-21@unima.ac.mw>
2024-11-30 16:45:34 +0200

    added notification related functions

--- app/(tabs)/(home)/notification.tsx
+++ app/(tabs)/(home)/notification.tsx
@@ -1,58 +1,94 @@
+import { registerForPushNotificationsAsync } from "@/functions/notifications";
 import { MaterialIcons } from "@expo/vector-icons";
+import { useContext, useEffect, useRef, useState } from "react";
 import { Text, View } from "react-native";
 import { Card, Title } from "react-native-paper";
+import * as Notifications from "expo-notifications";
+import { Platform } from "react-native";
+import { UserContext } from "@/hooks/useUserContext";
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function Notification() {
+  const {
+    expoPushToken,
+    setExpoPushToken,
+    channels,
+    setChannels,
+    notifications,
+    setNotifications,
+    notification,
+    setNotification,
+    notificationListener,
+    responseListener,
+  } = useContext(UserContext);
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(
+      (token) => token && setExpoPushToken(token)
+    );
+
+    if (Platform.OS === "android") {
+      Notifications.getNotificationChannelsAsync().then((value) =>
+        setChannels(value ?? [])
+      );
+    }
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+        setNotifications([...notifications, notification]);
+        console.log("Notification listener in notification: ", notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log("Response listener: in notification: ", response);
+      });
+
+    return () => {
+      notificationListener.current &&
+        Notifications.removeNotificationSubscription(
+          notificationListener.current
+        );
+      responseListener.current &&
+        Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
   return (
     <>
       <View className="h-full bg-gray-300 ">
-        <Card className="bg-white h-36 rounded-e-lg mx-4 my-3 ">
-          <View className="flex-row items-center p-3 ">
-            <MaterialIcons name="warning" size={24} />
-            <Title><Text> Alert</Text></Title>
-          </View>
-          <View>
-            <Text className="mx-2 text-s">
-              You have used 60 % of your budget. Please review and adjust where
-              necessary.
-            </Text>
-            <Text className="text-right text-s text-gray-600 mx-2">
-              13:32 pm
-            </Text>
-          </View>
-        </Card>
-
-        <Card className="bg-white h-36 rounded-e-lg mx-4 my-3 ">
-          <View className="flex-row items-center p-3 ">
-            <MaterialIcons name="warning" size={24} />
-            <Title><Text> Alert</Text></Title>
-          </View>
-          <View>
-            <Text className="mx-2 text-s">
-              You have exceeded your monthly expenses. Please review and adjust
-              your budget.
-            </Text>
-            <Text className="text-right text-s text-gray-600 mx-2">
-              13:32 pm
-            </Text>
-          </View>
-        </Card>
-        <Card className="bg-white h-36 rounded-e-lg mx-4 my-3">
-          <View className="flex-row items-center  p-3 ">
-            <MaterialIcons name="notifications" size={24} />
-            <Title><Text> Alert</Text></Title>
-          </View>
-          <View>
-            <Text className="mx-2 text-s">
-              You haven’t entered your expenses or income for today. Please
-              update your records.
-            </Text>
-            <Text className="text-right text-s text-gray-600 mx-2">
-              13:32 pm
-            </Text>
-          </View>
-        </Card>
+        {notifications.map((not) => {
+          return (
+            <Card className="bg-white h-36 rounded-e-lg mx-4 my-3 ">
+              <View className="flex-row items-center p-3 ">
+                <MaterialIcons name="warning" size={24} />
+                <Title>
+                  <Text> Alert: {not?.request.content.title}</Text>
+                </Title>
+              </View>
+              <View>
+                <Text className="mx-2 text-s">
+                  {notification && notification.request.content.body}
+                </Text>
+                <Text>
+                  Data:{" "}
+                  {notification &&
+                    JSON.stringify(notification.request.content.data)}
+                </Text>
+                <Text className="text-right text-s text-gray-600 mx-2">
+                  13:32 pm
+                </Text>
+              </View>
+            </Card>
+          );
+        })}
       </View>
     </>
   );
