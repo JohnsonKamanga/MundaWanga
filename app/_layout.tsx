2d06e2248decfb04e853506a6034c6b7b288d11e
1234567abule <bsc-45-21@unima.ac.mw>
2024-11-30 16:45:08 +0200

    added global notification context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,44 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useRef, useState } from "react";
+import "react-native-reanimated";
 import "../global.css";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-import { SQLiteProvider } from 'expo-sqlite';
-import { UserContext, useUserContext } from '../hooks/useUserContext';
-import { createTables } from '@/model/schema';
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { SQLiteProvider } from "expo-sqlite";
+import { UserContext, useUserContext } from "../hooks/useUserContext";
+import { createTables } from "@/model/schema";
+import * as Notifications from "expo-notifications";
+import { Platform } from "react-native";
+import { registerForPushNotificationsAsync } from "@/functions/notifications";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const {token, setToken} = useUserContext();
+  const { token, setToken } = useUserContext();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [channels, setChannels] = useState<Notifications.NotificationChannel[]>(
+    []
+  );
+  const [notification, setNotification] = useState<
+    Notifications.Notification | undefined
+  >(undefined);
+  const [notifications, setNotifications] = useState<
+    Notifications.Notification[]
+  >([]);
+  const notificationListener = useRef<Notifications.Subscription>();
+  const responseListener = useRef<Notifications.Subscription>();
 
   useEffect(() => {
     if (loaded) {
@@ -27,23 +46,37 @@ export default function RootLayout() {
     }
   }, [loaded]);
 
-
   if (!loaded) {
     return null;
   }
 
   return (
-    <SQLiteProvider databaseName='MundaWanga.db'>
-    <UserContext.Provider
-    value={{token, setToken}}
-    >
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
-    </UserContext.Provider>
+    <SQLiteProvider databaseName="MundaWanga.db">
+      <UserContext.Provider
+        value={{
+          token,
+          setToken,
+          expoPushToken,
+          setExpoPushToken,
+          channels,
+          setChannels,
+          notifications,
+          setNotifications,
+          notification,
+          setNotification,
+          notificationListener,
+          responseListener,
+        }}
+      >
+        <ThemeProvider
+          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+        >
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+        </ThemeProvider>
+      </UserContext.Provider>
     </SQLiteProvider>
   );
 }
--- hooks/useUserContext.tsx
+++ hooks/useUserContext.tsx
@@ -1,35 +1,68 @@
 import { getItem, setItem } from "expo-secure-store";
 import { createContext, useState } from "react";
+import * as Notifications from "expo-notifications";
 
 interface IToken {
-    access_token: string,
-    username: string
+  access_token: string;
+  username: string;
 }
 
-interface IUserContext{
-    setToken: React.Dispatch<React.SetStateAction<IToken | undefined>>;
-    token: IToken | undefined;
+interface IUserContext {
+  setToken: React.Dispatch<React.SetStateAction<IToken | undefined>>;
+  token: IToken | undefined;
 }
 
-export const UserContext = createContext<IUserContext>({
-    token: undefined,
-    setToken: ()=>{},
+interface SystemContext extends IUserContext {
+  expoPushToken: string;
+  setExpoPushToken: React.Dispatch<React.SetStateAction<string>>;
+  channels: Notifications.NotificationChannel[];
+  setChannels: React.Dispatch<
+    React.SetStateAction<Notifications.NotificationChannel[]>
+  >;
+  notifications: Notifications.Notification[];
+  setNotifications: React.Dispatch<
+    React.SetStateAction<Notifications.Notification[]>
+  >;
+  notification: Notifications.Notification | undefined;
+  setNotification: React.Dispatch<
+    React.SetStateAction<Notifications.Notification | undefined>
+  >;
+  notificationListener: React.MutableRefObject<
+    Notifications.Subscription | undefined
+  >;
+  responseListener: React.MutableRefObject<
+    Notifications.Subscription | undefined
+  >;
+}
+
+export const UserContext = createContext<SystemContext>({
+  token: undefined,
+  setToken: () => {},
+  expoPushToken: "",
+  setExpoPushToken: () => "",
+  channels: [],
+  setChannels: () => [],
+  notifications: [],
+  setNotifications: () => [],
+  notification: undefined,
+  setNotification: () => undefined,
+  notificationListener: { current: undefined },
+  responseListener: { current: undefined },
 });
 
+export function useUserContext() {
+  const getToken = () => {
+    const tokenString = getItem("token");
+    const userToken = tokenString ? JSON.parse(tokenString) : undefined;
+    return userToken;
+  };
 
-export function useUserContext(){
-    const getToken = ()=>{
-        const tokenString = getItem('token');
-        const userToken = tokenString? JSON.parse(tokenString) : undefined;
-        return userToken;
-    }
+  const [token, setToken] = useState<IToken | undefined>(getToken());
 
-    const [token, setToken] = useState<IToken | undefined>(getToken());
+  const saveToken = (userToken: React.SetStateAction<IToken | undefined>) => {
+    setItem("token", JSON.stringify(userToken));
+    setToken(userToken);
+  };
 
-    const saveToken = (userToken : React.SetStateAction<IToken | undefined>)=>{
-        setItem('token',JSON.stringify(userToken));
-        setToken(userToken);
-    }
-
-    return {token, setToken: saveToken};
+  return { token, setToken: saveToken };
 }
