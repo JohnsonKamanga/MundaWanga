6bb98fbb39cccd681cbb56a83ab176991074a164
JohnsonKamanga123 <bsc-com-09-21@unima.ac.mw>
2024-11-30 16:47:10 +0200

    added error handling to the expense update function

--- model/finances/expense.ts
+++ model/finances/expense.ts
@@ -2,7 +2,7 @@ import { SQLiteDatabase } from "expo-sqlite";
 import { ColumnMapping, columnTypes, Repository } from "expo-sqlite-orm";
 import { createExpenseTable } from "../schema";
 import { findInventoryById, updateInventory } from "../inventory/inventory";
-import { findBudgetRowById, updateBudget } from "./budget";
+import { findBudgetRowById, updateBudget, TBudget } from "./budget";
 
 export type TExpense = {
   id?: number;
@@ -36,7 +36,7 @@ export async function addexpense(
   expense: TExpense,
   db: SQLiteDatabase
 ): Promise<TExpense> {
-  
+  await createExpenseTable(db);
 
   //update related inventory
   if (expense.inventory_id) {
@@ -78,7 +78,7 @@ export async function addexpense(
         },
         db
       );
-      console.log('updated budget: ', res);
+      console.log("updated budget: ", res);
     }
   }
 
@@ -89,89 +89,107 @@ export async function findExpenseById(
   id: number,
   db: SQLiteDatabase
 ): Promise<TExpense | null> {
-  
-  return expenseRepository.findBy({ id: { equals: id } });
+  await createExpenseTable(db);
+
+  const expenses = await expenseRepository.query({
+    where: { id: { equals: id } },
+  });
+  return expenses[0];
 }
 
 export async function findAllExpenses(db: SQLiteDatabase): Promise<TExpense[]> {
-  
-
   return expenseRepository.query();
 }
 
 export async function updateExpense(
   expense: TExpense,
   db: SQLiteDatabase
-): Promise<TExpense | null> {
-  
-  const sql = `UPDATE expense 
+): Promise<TExpense | null > {
+
+    await createExpenseTable(db);
+
+    const sql = `UPDATE expense 
                  SET inventory_id = ?, last_modified = ?, quantity_used = ?, description = ? , amount_of_money = ?
                  WHERE id = ?`;
-  const params = [
-    expense.inventory_id,
-    expense.last_modified,
-    expense.quantity_used,
-    expense.description,
-    expense.amount_of_money,
-    expense.id,
-  ];
+    const params = [
+      expense.inventory_id,
+      expense.last_modified,
+      expense.quantity_used,
+      expense.description,
+      expense.amount_of_money,
+      expense.id,
+    ];
 
-  if (expense.inventory_id && expense.id) {
-    const targetInventory = await findInventoryById(expense.inventory_id, db);
-    const targetExpense = await findExpenseById(expense.id, db);
-    if (targetInventory && targetInventory.id && targetExpense) {
-      const res = await updateInventory(
-        {
-          last_modified: Date.now(),
-          available_quantity:
-            targetInventory.available_quantity -
-            (targetExpense.quantity_used - expense.quantity_used),
-          name: targetInventory.name,
-          id: targetInventory.id,
-        },
-        db
-      );
-      console.log('updated inventory: ', res);
+    if (expense.inventory_id && expense.id) {
+
+      const targetInventory = await findInventoryById(expense.inventory_id, db);
+      const targetExpense = await findExpenseById(expense.id, db);
+      if (targetInventory && targetInventory.id && targetExpense) {
+        await updateInventory(
+          {
+            last_modified: Date.now(),
+            available_quantity:
+              targetInventory.available_quantity -
+              (targetExpense.quantity_used - expense.quantity_used),
+            name: targetInventory.name,
+            id: targetInventory.id,
+          },
+          db
+        );
+      }
     }
-  }
 
-  //updated related budget
-  if (expense.budget_id && expense.id) {
-    const targetBudget = await findBudgetRowById(expense.budget_id, db);
-    const targetExpense = await findExpenseById(expense.id, db);
-
-    if (
-      targetBudget &&
-      targetBudget.id &&
-      targetBudget.used &&
-      targetBudget.set_date &&
-      targetExpense &&
-      targetExpense.amount_of_money
-    ) {
-      const res = await updateBudget(
-        {
-          last_modified: Date.now(),
-          name: targetBudget.name,
-          used: targetBudget.used + (expense.amount_of_money - targetExpense.amount_of_money),
-          id: targetBudget.id,
-          max_amount: targetBudget.max_amount,
-          end_date: targetBudget.end_date,
-          set_date: targetBudget.set_date,
-        },
-        db
+    //updated related budget
+    if (expense.budget_id && expense.id) {
+      const targetBudget = (
+        await findBudgetRowById(expense.budget_id, db)
       );
-      console.log('updated budget: ', res);
-    }
-  }
 
-  return expenseRepository.databaseLayer.executeSql(sql, params);
+      const targetExpense = await findExpenseById(expense.id, db);
+      const newval =
+        targetBudget?.used +
+        (expense.amount_of_money - targetExpense?.amount_of_money);
+        console.log("new used amount: ", newval)
+        if (newval < 0) {
+          throw new RangeError("New amount of money added should be greater than or equal to current amount of money");
+        }
+
+        if (targetBudget.max_amount - newval < 0) {
+          throw new RangeError("New amount of money added exceeds your budget");
+        }
+
+        if (
+          targetBudget &&
+          targetBudget.id &&
+          targetExpense &&
+          targetExpense.amount_of_money
+        ) {
+
+           await updateBudget(
+            {
+              last_modified: Date.now(),
+              name: targetBudget.name,
+              used: newval,
+              id: targetBudget.id,
+              max_amount: targetBudget.max_amount,
+              end_date: targetBudget.end_date,
+              set_date: targetBudget.set_date,
+            },
+            db
+          );
+      }
+    }
+
+    return expenseRepository.databaseLayer.executeSql(sql, params);
+    
+  
 }
 
 export async function deleteExpense(
   id: number,
   db: SQLiteDatabase
 ): Promise<boolean> {
-  
+  await createExpenseTable(db);
 
   return expenseRepository.destroy(id);
 }
