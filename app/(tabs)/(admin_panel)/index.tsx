83a8f1ce21c074b691894d0593e68e455b6d1e71
JohnsonKamanga123 <bsc-com-09-21@unima.ac.mw>
2024-11-30 21:41:47 +0200

    connected the admin feedback component to the backend

--- app/(tabs)/(admin_panel)/index.tsx
+++ app/(tabs)/(admin_panel)/index.tsx
@@ -2,29 +2,57 @@ import { View, Text } from "react-native";
 import { useColorScheme } from "react-native"
 import { ScrollView } from "react-native-gesture-handler";
 import { Colors } from "@/constants/Colors"
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { baseurl } from "@/constants/url";
 
 
 export default function AdminFeedBack(){
-    
+    const [feedbackArray, setFeedbackArray] = useState([]);
     const colorScheme = useColorScheme();
     const color = Colors[colorScheme ?? 'light']
 
 
+    useEffect(()=>{
+        axios.get(`${baseurl}/feedback`)
+        .then((fetchedData)=>{
+            setFeedbackArray(fetchedData.data);
+        })
+    },[])
+
     return(
         <>
             <ScrollView>
-            <View style={{
+            {
+                feedbackArray.length > 0 ?
+                feedbackArray.map((item)=>(
+            <View key={item?.id} style={{
                 backgroundColor: color.background,}} className="w-full">
                 <View style={{
                     backgroundColor: 'white',
                     borderColor: color.borderColor,
                     margin: 10,
                 }} className="border-solid border rounded-xl mx-4 w-11/12">
-                    <Text className="text-lg font-bold m-2 text-center">John Doe</Text>
-                    <Text className="mx-3 text-center font-semibold text-sm mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel lorem eu ex varius convallis. Sed ut nulla a nisl efficitur tincidunt sit amet vitae justo. Curabitur tristique eros sit amet neque vestibulum, vel convallis nunc fermentum. Quisque vel consequat sapien. Suspendisse potenti. Nam ut accumsan augue, vel egestas eros. Fusce consequat, odio nec tristique dignissim, libero sapien porttitor felis.
+                    <Text className="text-lg font-bold m-2 text-center">{item?.username}</Text>
+                    <Text className="mx-3 text-center font-semibold text-sm mb-4">
+                        {item?.message}
                     </Text>
                 </View>
-            </View>
+            </View>))
+        :
+        <View  style={{
+            backgroundColor: color.background,}} className="w-full">
+                <View style={{
+                    backgroundColor: 'white',
+                    borderColor: color.borderColor,
+                    margin: 10,
+                }} className="border-solid border rounded-xl mx-4 w-11/12 items-center justify-center min-h-[200px]">
+                    <Text>
+                        No Feedback available
+                    </Text>
+                </View>
+        </View>    
+        }
             </ScrollView>
         </>
     )
